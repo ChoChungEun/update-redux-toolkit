f1e90154e1e1dbe88a731c837b7d297f0340fc08
ChoChungEun <82curst@gmail.com>
2024-01-25 11:55:30 +0900

    linked state to profile page

--- src/redux/user.js
+++ src/redux/user.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    value: { name: "", age: "", email: "" },
+    value: { name: "", age: "0", email: "" },
   },
   reducers: {
     login: (state, action) => {
